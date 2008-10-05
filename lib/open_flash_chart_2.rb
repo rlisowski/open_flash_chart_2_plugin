2f29d69485f9fcc83a5568fd13629e8c08acb23f
Rafał <lisukorin@gmail.com>
2008-10-05 10:56:32 +0200

    add some documentation and make clean up old way generating instance variables getters and setters

--- lib/open_flash_chart_2.rb
+++ lib/open_flash_chart_2.rb
@@ -24,105 +24,135 @@ module OFC2
       end
     end
   end
+
   def self.included(controller)
     controller.helper_method(:ofc2, :ofc2_inline)
   end
+
+  # generate a ofc object using Graph object
+  # +width+ width for div
+  # +height+ height for div
+  # +graph+ a OFC2::Graph object
+  # +base+ uri for graph, default '/'
+  # +id+ id for div with graph, default Time.now.usec
   def ofc2_inline(width, height, graph, base='/', id=Time.now.usec)
     # TODO: generating more than one graph with ofc2_inline on the same page is currently impossible
     div_name = "flashcontent_#{id}"
-    out = []
-    out << '<div id="' + div_name.to_s + '"></div>'
-    out <<  <<-EOF
-<script type="text/javascript">
-#{div_name} = '#{graph.render}';
-function open_flash_chart_data(){
-return #{div_name};
-};
-// i'm not shure that is necessary
-function findSWF(movieName) {
-if (navigator.appName.indexOf("Microsoft")!= -1) {
-return window[movieName];
-} else {
-return document[movieName];
-}
-};
-swfobject.embedSWF(
-'#{base}open-flash-chart.swf', '#{div_name}',
-'#{width}', '#{height}','9.0.0', 'expressInstall.swf'
-);
-</script>
+    <<-EOF
+      <div id="#{div_name}"></div>
+      <script type="text/javascript">
+        #{div_name} = '#{graph.render}';
+
+        function open_flash_chart_data(){
+          return #{div_name};
+        };
+
+        // i'm not shure that is necessary
+        function findSWF(movieName) {
+          if (navigator.appName.indexOf("Microsoft")!= -1) {
+            return window[movieName];
+          } else {
+            return document[movieName];
+          }
+        };
+
+        swfobject.embedSWF(
+          '#{base}open-flash-chart.swf', '#{div_name}',
+          '#{width}', '#{height}','9.0.0', 'expressInstall.swf'
+        );
+      </script>
     EOF
-    return out.join("\n")
   end
+
+  # generate a ofc object using data from url
+  # +width+ width for div
+  # +height+ height for div
+  # +url+ an url which return data in json format
+  # +base+ uri for graph, default '/'
+  # +id+ id for div with graph, default Time.now.usec
   def ofc2(width, height, url, base='/', id =Time.now.usec)
-    out = []
     div_name = "flashcontent_#{id}"
-    out << '<div id="' + div_name.to_s + '"></div>'
-    out << '<script type="text/javascript">'
-    out << 'swfobject.embedSWF('
-    out << '"' + base.to_s + 'open-flash-chart.swf", "' + div_name.to_s + '",'
-    out << '"' + width.to_s + '", "' + height.to_s + '", "9.0.0", "expressInstall.swf",'
-    out << '{"data-file":"' + base.to_s + url.to_s + '"} );'
-    out << '</script>'
-    return out.join("\n")
+    <<-EOF
+      <div id='#{div_name}'></div>
+      <script type="text/javascript">
+        swfobject.embedSWF(
+        "#{base}open-flash-chart.swf","#{div_name}",
+        "#{width}", "#{height}", "9.0.0", "expressInstall.swf",
+        {"data-file":"#{base}#{url}"} );
+      </script>
+    EOF
   end
+
+=begin
+  insance variables:
+  +style+ :style for element, it's is in css style eg. "{font-size: 20px; color: #FF0F0F; text-align: center;}"
+  +text+ :text for element
+=end
   class Element
     include OWJSON
     def initialize(text = '', css = "{font-size: 20px; color: #FF0F0F; text-align: center;}")
       set_text(text)
       set_style(css)
     end
-    # insance variables:
-    # style text
-    #    %w(style text).each do |method|
-    #      define_method("set_#{method}") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}=") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}") do
-    #        self.instance_variable_get("@#{method.gsub('_','__')}")
-    #      end
-    #    end
   end
-  class XLegend <  Element
-  end
-  class Title < Element
-  end
-  class YLegend <  Element
-  end
-  # y_axis
+
+=begin
+  documentation is the same as Element class
+=end
+  class XLegend <  Element ;end
+=begin
+  documentation is the same as Element class
+=end
+  class Title < Element ;end
+=begin
+  documentation is the same as Element class
+=end
+  class YLegend <  Element ;end
+
+
+=begin
+  y_axis
+  +stroke+
+  +tick_length+
+  +colour+
+  +min+
+  +max+
+  +steps+
+  +labels+
+=end
   class YAxisBase
     include OWJSON
-    # instance variables
-    # stroke tick_length colour min max steps labels
-    #    %w(stroke tick_length colour min max steps labels).each do |method|
-    #      define_method("set_#{method}") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}=") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}") do
-    #        self.instance_variable_get("@#{method.gsub('_','__')}")
-    #      end
-    #    end
+
+    # set colour and grid_colour at once
+    # there is also an alias colours=
+    # +colour+ colour for labels eg. #FF0000
+    # +grid_colour+ colour for grid eg. #00FF00
     def set_colours( colour, grid_colour )
       set_colour( colour )
       set_grid_colour( grid_colour )
     end
     alias_method :colours=, :set_colours
+
+
+    # set range at once
+    # there is also an alias range=
+    # +min+ minimum for y_axis
+    # +max+ maximum for y_axis
+    # +steps+ how many steps skip before print label
     def set_range( min, max, steps=1 )
       set_min(min)
       set_max(max)
       set_steps( steps )
     end
     alias_method :range=, :set_range
+
+    # set offset for axis, these is handy when You want to 3d graph render
+    # there is also an alias offset=
     def set_offset( off )
       @offset = off ? 1 : 0
     end
     alias_method :offset=, :set_offset
+
   end
   class YAxis < YAxisBase
     # left axis control grid colour, but right not
@@ -133,22 +163,23 @@ swfobject.embedSWF(
   end
   class YAxisRight < YAxisBase
   end
-  # x_axis
+
+=begin
+  x_axis
+  +stroke+
+  +tick_length+
+  +colour+
+  +tick_height+
+  +grid_colour+
+  +min+
+  +max+
+  +steps+
+  +labels+
+  +offset+
+=end
   class XAxis
     include OWJSON
-    # instance variables
-    # stroke tick_length colour tick_height grid_colour min max steps labels offset
-    #    %w(stroke tick_length colour tick_height grid_colour min max steps labels offset).each do |method|
-    #      define_method("set_#{method}") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}=") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}") do
-    #        self.instance_variable_get("@#{method.gsub('_','__')}")
-    #      end
-    #    end
+
     # well it must be done this way because instance variable name can't be started by number
     %w(3d).each do |method|
       define_method("set_#{method}") do |a|
@@ -165,6 +196,7 @@ swfobject.embedSWF(
       set_colour( colour )
       set_grid_colour( grid_colour )
     end
+
     # o is treat as a logic
     def set_offset( o )
       @offset = o ? true : false
@@ -273,36 +305,64 @@ swfobject.embedSWF(
     #      end
     #    end
   end
+
+
+=begin
+  +title+
+  +x_axis+
+  +y_axis+
+  +y_axis_right+
+  +x_legend+
+  +y_legend+
+  +bg_colour+
+  +elements+
+=end
   class Graph
     include OWJSON
-    # instance variables
-    # title x_axis y_axis y_axis_right x_legend y_legend bg_colour elements
-    #    %w(title x_axis y_axis y_axis_right x_legend y_legend bg_colour elements).each do |method|
-    #      define_method("set_#{method}") do |a|
-    #        self.instance_variable_set("@#{method}", a)
-    #      end
-    #      define_method("#{method}=") do |a|
-    #        self.instance_variable_set("@#{method}", a)
-    #      end
-    #      define_method("#{method}") do
-    #        self.instance_variable_get("@#{method}")
-    #      end
-    #    end
+
+    # it must be done in that way because method_miising method replace _ to __
+    %w(x_axis y_axis y_axis_right x_legend y_legend bg_colour).each do |method|
+      define_method("set_#{method}") do |a|
+        self.instance_variable_set("@#{method}", a)
+      end
+      define_method("#{method}=") do |a|
+        self.instance_variable_set("@#{method}", a)
+      end
+      define_method("#{method}") do
+        self.instance_variable_get("@#{method}")
+      end
+    end
+
     def initialize
       @title = Title.new( "Graph" )
       @elements = []
     end
+
     def add_element( e )
       @elements << e
     end
     alias_method :<<, :add_element
+
     def render
       s = to_json
-      s = s.gsub('___','')
-      s.gsub('__','-')
+      # everything about underscores
+      s.gsub!('___','') # that is for @___3d variable
+      s.gsub!('__','-') # that is for @smt__smt variables
+      # variables @smt_smt should look without changes
+      s
     end
   end
-  # line chart
+
+=begin
+  line chart
+  +values+
+  +width+
+  +colour+
+  +font_size+
+  +dot_size+
+  +halo_size+
+  +text+
+=end
   class LineBase
     include OWJSON
     def initialize(text = 'label text', font_size='10px', values = [9,6,7,9,5,7,6,9,7])
@@ -311,34 +371,42 @@ swfobject.embedSWF(
       @font__size = font_size
       @values    = values
     end
-    # values width colour font_size dot_size halo_size text
-    #    %w(values width colour font_size dot_size halo_size text).each do |method|
-    #      define_method("set_#{method}") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}=") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}") do
-    #        self.instance_variable_get("@#{method.gsub('_','__')}")
-    #      end
-    #    end
   end
-  class LineDot < LineBase
-  end
+
+=begin
+  go to class LineBase for details
+=end
+  class LineDot < LineBase ;end
+
+=begin
+  go to class LineBase for details
+=end
   class Line < LineBase
     def initialize
       super
       @type      = "line"
     end
   end
+
+=begin
+  go to class LineBase for details
+=end
   class LineHollow < LineBase
     def initialize
       super
       @type      = "line_hollow"
     end
   end
-  #area
+
+=begin
+  +width+
+  +color+
+  +values+
+  +dot_size+
+  +text+
+  +font_size+
+  +fill_alpha+
+=end
   class AreaHollow
     include OWJSON
     def initialize(fill_alpha = 0.35, values = [])
@@ -346,20 +414,15 @@ swfobject.embedSWF(
       set_fill_alpha  fill_alpha
       @values    = values
     end
-    # width color values dot_size text font_size fill_alpha
-    #    %w(width color values dot_size text font_size fill_alpha).each do |method|
-    #      define_method("set_#{method}") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}=") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}") do
-    #        self.instance_variable_get("@#{method.gsub('_','__')}")
-    #      end
-    #    end
   end
-  #bar
+
+=begin
+  +alpha+
+  +colour+
+  +values+
+  +text+
+  +font_size+
+=end
   class BarBase
     include OWJSON
     def initialize (values = [], text = '', size = '10px')
@@ -367,32 +430,30 @@ swfobject.embedSWF(
       @text = text
       @font__size = size
     end
-    def set_key( text, size )
+    def set_key( text, size )s
       @text = text
       @font__size = size
     end
-    # alpha colour values text font_size
-    #    %w(alpha colour values text font_size).each do |method|
-    #      define_method("set_#{method}") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}=") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}") do
-    #        self.instance_variable_get("@#{method.gsub('_','__')}")
-    #      end
-    #    end
     def append_value( v )
       @values << v
     end
     alias_method :<<, :append_value
   end
+
+=begin
+  go to class BarBase for details
+=end
   class Bar < BarBase
     def initialize
       @type      = "bar"
     end
   end
+
+=begin
+  +top+
+  +colour+
+  +tip+
+=end
   class Value
     include OWJSON
     def initialize(top = 0, color = '', tip = nil)
@@ -400,18 +461,6 @@ swfobject.embedSWF(
       @colour = color
       @tip = tip
     end
-    #top colour tip
-    #    %w(top colour tip).each do |method|
-    #      define_method("set_#{method}") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}=") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}") do
-    #        self.instance_variable_get("@#{method.gsub('_','__')}")
-    #      end
-    #    end
     def set_tooltip( tip )
       @tip = tip
     end
@@ -421,11 +470,22 @@ swfobject.embedSWF(
       @type      = "bar_3d"
     end
   end
+
+=begin
+ go to class BarBase documentation for details
+=end
   class BarGlass < BarBase
     def initialize()
       @type      = "bar_glass"
     end
   end
+
+=begin
+ go to class BarBase documentation for details
+  +offset+
+  +colour+
+  +outline_colour+
+=end
   class BarSketch < BarBase
     def initialize( colour = '#ff0000', outline_colour = '#00FF00', fun_factor = 5)
       @type      = "bar_sketch"
@@ -433,19 +493,11 @@ swfobject.embedSWF(
       set_outline_colour( outline_colour )
       @offset = fun_factor
     end
-    # offset outline_colour
-    #    %w(offset outline_colour).each do |method|
-    #      define_method("set_#{method}") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}=") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}") do
-    #        self.instance_variable_get("@#{method.gsub('_','__')}")
-    #      end
-    #    end
   end
+
+=begin
+ go to class BarBase documentation for details
+=end
   class BarStack < BarBase
     include OWJSON
     def initialize
@@ -454,44 +506,38 @@ swfobject.embedSWF(
     end
     alias_method :append_stack, :append_value
   end
+
+=begin
+ go to class Value documentation for details
+  +val+
+  +color+
+=end
   class BarStackValue < Value
     include OWJSON
     def initialize(val, colour)
       @val = val
       @colour = colour
     end
-    # val color
-    #    %w(val color).each do |method|
-    #      define_method("set_#{method}") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}=") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}") do
-    #        self.instance_variable_get("@#{method.gsub('_','__')}")
-    #      end
-    #    end
   end
+
+=begin
+  +left+
+  +right+
+=end
   class HBarValue
     include OWJSON
     def initialize( left, right )
       @left = left
       @right = right
     end
-    # left right
-    #    %w(left right).each do |method|
-    #      define_method("set_#{method}") do |a|
-    #        self.instance_variable_set("@#{method}", a)
-    #      end
-    #      define_method("#{method}=") do |a|
-    #        self.instance_variable_set("@#{method}", a)
-    #      end
-    #      define_method("#{method}") do
-    #        self.instance_variable_get("@#{method.gsub('_','__')}")
-    #      end
-    #    end
   end
+
+=begin
+  +colour+
+  +text+
+  +font_size+
+  +values+
+=end
   class HBar
     include OWJSON
     def initialize(colour = "#9933CC", text = '', font_size = '10px')
@@ -501,52 +547,39 @@ swfobject.embedSWF(
       set_font_size font_size
       @values    = []
     end
-    # colour text font_size values
-    #    %w(colour text font_size values).each do |method|
-    #      define_method("set_#{method}") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}=") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}") do
-    #        self.instance_variable_get("@#{method.gsub('_','__')}")
-    #      end
-    #    end
+
     # v suppostu be HBarValue class
     def append_value( v )
       @values << v
     end
     alias_method :<<, :append_value
   end
+
   ########## pie
   class PieValue
     include OWJSON
     def initialize( value, text )
-      @value = value
-      @text = text
+      @value  = value
+      @text   = text
     end
   end
+
+=begin
+  +colours+
+  +alpha+
+  +border+
+  +values+
+  +animate+
+  +start_angle+
+=end
   class Pie
     include OWJSON
     def initialize(colours = ["#d01f3c","#356aa0","#C79810"], alpha = 0.6, border = 2, values = [2,3, PieValue.new(6.5, "hello (6.5)")])
-      @type      		= 'pie'
-      @colours     		= colours
-      @alpha			= alpha
-      @border			= border
-      @values			= values
+      @type     = 'pie'
+      @colours  = colours
+      @alpha	= alpha
+      @border	= border
+      @values	= values
     end
-    # colours alpha border values animate start_angle
-    #    %w(colours alpha border values animate start_angle).each do |method|
-    #      define_method("set_#{method}") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}=") do |a|
-    #        self.instance_variable_set("@#{method.gsub('_','__')}", a)
-    #      end
-    #      define_method("#{method}") do
-    #        self.instance_variable_get("@#{method.gsub('_','__')}")
-    #      end
-    #    end
   end
 end
